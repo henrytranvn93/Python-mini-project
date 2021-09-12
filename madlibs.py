d300075db5287c6bb4bf39a0825011a63a2c5a58
henrytranvn93 <henry.tran@htmi.ch>
2021-09-11 17:15:42 -0700

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# youtuber = "Henry Tran"
+
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programing is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
