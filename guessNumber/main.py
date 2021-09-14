a13dced573c6b413f2375364477e1d553b633e2d
DESKTOP-G34PGPB\Kieu Tran <henry.tran@htmi.ch>
2021-09-13 21:46:42 -0700

    guessing game initial

---
+++ guessNumber/main.py
@@ -0,0 +1,9 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+
+guess(10)
