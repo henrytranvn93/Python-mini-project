f54d6c144e7ee4804b1b836d097c74ea7b0c5270
henrytranvn93 <henry.tran@htmi.ch>
2021-09-16 15:36:37 -0700

    add computer guess function into guessNumber game

--- guessNumber/main.py
+++ guessNumber/main.py
@@ -12,4 +12,21 @@ def guess(x):
 
     print(f'Yay, you got the jackpot. The number is {random_number}')
 
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low!= high:
+            guess = random.randint(low,high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too High (H), too low (L), or correct (C)??').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Yay! The computer guessed your number. {guess}, correctly!')
+
+computer_guess(10)
