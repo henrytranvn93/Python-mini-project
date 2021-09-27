cfe96705579f8c0c087999651a47748f9b2090de
henrytranvn93 <henry.tran@htmi.ch>
2021-09-26 19:02:55 -0700

    add lives functions and adjust some comments

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -15,25 +15,36 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set() #what user has guessed
 
-    while len(word_letters) > 0:
+    lives = 6
+
+    while len(word_letters) > 0 and lives > 0:
+
+        print(f'You have {lives} lives left')
 
         print('You have used these letters: ', ' '.join(used_letters))
         word_list = [letter if letter in used_letters else '-' for letter in word]
 
-        print('Current word: ', ' '.join(used_letters))
+        print('Current word: ', ' '.join(word_list))
 
         user_letter = input('Guess a letter: ').upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            
+            else: 
+                lives = lives - 1
+                print('Letter is not in word.')
 
         elif user_letter in used_letters:
             print('You already guessed that character')
         
         else:
             print('Invalid character')
+    if lives == 0: 
+        print(f'You died!!! The word was {word}')
+    else:
+        print(f'You guessed the word {word}!!! Congrats!!!')
         
 
-user_input = input('Type something: ')
-print(user_input)
+hangman()
